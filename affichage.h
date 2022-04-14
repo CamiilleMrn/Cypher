f459c0de2db268c00d3b63bdcd4cc915011e1fee
Cam <marion_camille@yahoo.com>
2022-04-14 09:48:58 +0200

    test

---
+++ affichage.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+/*  Entrée : chaine (chaine de caractère à afficher après décodage ou encodage), int (valeur permettant de choisir ce qu'on doit afficher)
+    Affiche le message décoder ou encoder par l'algorithme de césar
+*/
+void affichage (char chaine[],int i);
+
+void presentation();
+
+void choixCrypt();
