cbb73ae79ea2af98d085238d4a10c7a5efb68e31
Cam <marion_camille@yahoo.com>
2022-04-14 10:27:23 +0200

    First Input

--- affichage.h
+++ affichage.h
@@ -1,3 +1,28 @@
+/******************************************************************************
+*  ASR => 4R2.04                                                              *
+*******************************************************************************
+*                                                                             *
+*  N° de Sujet :   3                                                          *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Intitulé :    Chiffrement de message                                       *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom-prénom1 :  MOLE Alexandre                                              *
+*                                                                             *
+*  Nom-prénom2 :  LECLERC Benoit                                              *
+*                                                                             *
+*  Nom-prénom3 :  MARION Camille                                              *
+*                                                                             *
+*  Nom-prénom4 :  EDELIN Louis                                                *
+*                                                                             *
+*******************************************************************************
+*                                                                             *
+*  Nom du fichier : affichage.h                                               *
+*                                                                             *
+******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 
@@ -6,6 +31,10 @@
 */
 void affichage (char chaine[],int i);
 
+/* Affiche graphiquement le message de choix de la technique de cryptage
+*/
 void presentation();
 
+/* Affiche graphiquement le message de choix : encodage ou decodage
+*/
 void choixCrypt();
